d6ece6857ecdfc03d58a0ac104e0f8b10b0481d9
Erik Lindstrom <elindstrom42@gmail.com>
2020-06-20 13:42:14 -0500

    removed useless comments

--- src/main.js
+++ src/main.js
@@ -8,26 +8,6 @@ const kennelLog = (name,breed) => {
     BowWowKennels.push(newDog)
 }
 
-// const packOfPets = kennelClub(pet1,pet2,pet3)
-
-// console.log(packOfPets)
-
-
-
-
-// const pet1 = {
-//     name: "Spike",
-//     breed: "Belgian Malinois"
-// }
-
-// const pet2 = {
-//     name: "Rex",
-//     breed: "Rodesian Ridgeback"
-// }
-// const pet3 = {
-//     name: "Spike",
-//     breed: "Rottweiler"
-// }
 
 kennelLog("Spike","Belgian Malinois")
 kennelLog("Rex","Rodesian Ridgeback")
